bf5cf8cc9dc4bc53a08d6571267e204b900e3d11
Ruthvik Allu <R.Allu@student.tudelft.nl>
2023-03-09 20:46:07 +0200

    cs

--- server/src/test/java/server/api/TestCardRepository.java
+++ server/src/test/java/server/api/TestCardRepository.java
@@ -1,4 +1,4 @@
-// checkstyle:off
+
 package server.api;
 
 import org.springframework.data.domain.Example;
@@ -13,23 +13,33 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Function;
 
-@SuppressWarnings("checkstyle:*")
 public class TestCardRepository implements CardRepository {
-    @SuppressWarnings("checkstyle.*")
     private final List<Card> cards;
-    @SuppressWarnings("checkstyle.*")
-    public final List<String> calledMethods = new ArrayList<>();
+    private final List<String> calledMethods = new ArrayList<>();
 
+    /**
+     * @param name
+     */
     @SuppressWarnings("checkstyle:MissingJavadocMethod")
     private void call(String name) {
         calledMethods.add(name);
     }
+
+    /**
+     *
+     */
     @SuppressWarnings("checkstyle.*")
     public TestCardRepository() {
         cards = new ArrayList<>();
         cards.add(new Card("Test1", 1));
         cards.add(new Card("Test2", 1));
     }
+
+    /**
+     * @param entity must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @SuppressWarnings("checkstyle.*")
     @Override
     public <S extends Card> S save(S entity) {
@@ -38,6 +48,11 @@ public class TestCardRepository implements CardRepository {
         cards.add(entity);
         return entity;
     }
+
+    /**
+     * @param id must not be {@literal null}.
+     * @return
+     */
     @SuppressWarnings("checkstyle.*")
     @Override
     public Optional<Card> findById(Long id) {
@@ -47,112 +62,209 @@ public class TestCardRepository implements CardRepository {
             return Optional.empty();
         }
     }
+
+    /**
+     * @return
+     */
     @SuppressWarnings("checkstyle.*")
     @Override
     public List<Card> findAll() {
         return cards;
     }
 
+    /**
+     * @param sort
+     * @return
+     */
     @Override
     public List<Card> findAll(Sort sort) {
         return null;
     }
 
+    /**
+     * @param pageable
+     * @return
+     */
     @Override
     public Page<Card> findAll(Pageable pageable) {
         return null;
     }
 
+    /**
+     * @param longs must not be {@literal null} nor contain any {@literal null} values.
+     * @return
+     */
     @Override
     public List<Card> findAllById(Iterable<Long> longs) {
         return null;
     }
 
+    /**
+     * @return
+     */
     @Override
     public long count() {
         return 0;
     }
 
+    /**
+     * @param entities must not be {@literal null} nor must it contain {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> List<S> saveAll(Iterable<S> entities) {
         return null;
     }
 
+    /**
+     *
+     */
     @Override
     public void flush() {
 
     }
 
+    /**
+     * @param entity entity to be saved. Must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> S saveAndFlush(S entity) {
         return null;
     }
 
+    /**
+     * @param entities entities to be saved. Must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> List<S> saveAllAndFlush(Iterable<S> entities) {
         return null;
     }
 
+    /**
+     * @param entities entities to be deleted. Must not be {@literal null}.
+     */
     @Override
     public void deleteAllInBatch(Iterable<Card> entities) {
 
     }
 
+    /**
+     * @param longs the ids of the entities to be deleted. Must not be {@literal null}.
+     */
     @Override
     public void deleteAllByIdInBatch(Iterable<Long> longs) {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void deleteAllInBatch() {
 
     }
 
+    /**
+     * @param aLong must not be {@literal null}.
+     * @return
+     */
     @Override
     public Card getOne(Long aLong) {
         return null;
     }
 
+    /**
+     * @param aLong must not be {@literal null}.
+     * @return
+     */
     @Override
     public Card getById(Long aLong) {
         return null;
     }
 
+    /**
+     * @param example must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> Optional<S> findOne(Example<S> example) {
         return Optional.empty();
     }
 
+    /**
+     * @param example must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> List<S> findAll(Example<S> example) {
         return null;
     }
 
+    /**
+     * @param example must not be {@literal null}.
+     * @param sort    the {@link Sort} specification to sort the results by, must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> List<S> findAll(Example<S> example, Sort sort) {
         return null;
     }
 
+    /**
+     * @param example  must not be {@literal null}.
+     * @param pageable can be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> Page<S> findAll(Example<S> example, Pageable pageable) {
         return null;
     }
 
+    /**
+     * @param example the {@link Example} to count instances for. Must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> long count(Example<S> example) {
         return 0;
     }
 
+    /**
+     * @param example the {@link Example} to use for the existence check. Must not be {@literal null}.
+     * @param <S>
+     * @return
+     */
     @Override
     public <S extends Card> boolean exists(Example<S> example) {
         return false;
     }
 
+    /**
+     * @param example       must not be {@literal null}.
+     * @param queryFunction the query function defining projection, sorting, and the result type
+     * @param <S>
+     * @param <R>
+     * @return
+     */
     @Override
-    public <S extends Card, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R> queryFunction) {
+    public <S extends Card, R> R findBy(Example<S> example, Function<FluentQuery.FetchableFluentQuery<S>, R>
+            queryFunction) {
         return null;
     }
 
+    /**
+     * @param id must not be {@literal null}.
+     */
     @Override
     public void deleteById(Long id) {
         if (id > 0 && id <= cards.size()) {
@@ -160,26 +272,42 @@ public class TestCardRepository implements CardRepository {
         }
     }
 
+    /**
+     * @param entity must not be {@literal null}.
+     */
     @Override
     public void delete(Card entity) {
 
     }
 
+    /**
+     * @param longs must not be {@literal null}. Must not contain {@literal null} elements.
+     */
     @Override
     public void deleteAllById(Iterable<? extends Long> longs) {
 
     }
 
+    /**
+     * @param entities must not be {@literal null}. Must not contain {@literal null} elements.
+     */
     @Override
     public void deleteAll(Iterable<? extends Card> entities) {
 
     }
 
+    /**
+     *
+     */
     @Override
     public void deleteAll() {
 
     }
 
+    /**
+     * @param id must not be {@literal null}. 
+     * @return
+     */
     @Override
     public boolean existsById(Long id) {
         if (id > 0 && id <= cards.size()) {
@@ -189,4 +317,3 @@ public class TestCardRepository implements CardRepository {
         }
     }
 }
-// checkstyle:on
