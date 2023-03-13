0a8ac9879f7a2697d7a0068bf55353ab3c44126e
Benjamin Sherlock <B.A.sherlock@student.tudelft.nl>
2023-03-13 15:13:47 +0100

    added java documentation to the resources directory of the server class, then I added a controller which can open the documentation with /docs.

---
+++ server/src/main/resources/public/documentation/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"client"},{"l":"client.scenes"},{"l":"client.utils"},{"l":"commons"},{"l":"server"},{"l":"server.api"},{"l":"server.database"}];updateSearchResults();
