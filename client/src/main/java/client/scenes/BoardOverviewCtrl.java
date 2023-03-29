d11ce04008daba943d26a4a7f6e2679e692da0c3
Stefan <bud@student.tudelft.nl>
2023-03-29 16:33:42 +0200

    Remove unused import

--- client/src/main/java/client/scenes/BoardOverviewCtrl.java
+++ client/src/main/java/client/scenes/BoardOverviewCtrl.java
@@ -13,7 +13,6 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
