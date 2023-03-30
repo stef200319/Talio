2e0b81a11f5eb0b03d4411eca04cf94a5b5791ac
Thijs van der Valk <t.c.j.vandervalk@student.tudelft.nl>
2023-03-30 09:52:28 +0200

    Added tests for the CardTag entity

--- commons/src/main/java/commons/CardTag.java
+++ commons/src/main/java/commons/CardTag.java
@@ -4,6 +4,7 @@ package commons;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import java.util.Objects;
 
 @Entity
 public class CardTag extends Tag {
@@ -37,4 +38,24 @@ public class CardTag extends Tag {
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CardTag)) return false;
+        if (!super.equals(o)) return false;
+        CardTag cardTag = (CardTag) o;
+        return Objects.equals(getBoard(), cardTag.getBoard());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), getBoard());
+    }
+
+    @Override
+    public String toString() {
+        return "CardTag{" +
+                "board=" + board +
+                '}';
+    }
 }
