729552359d031004b46185294dd0156a9d9a6114
Benjamin Sherlock <b.a.sherlock@student.tudelft.nl>
2023-04-06 12:06:00 +0200

    Revert "Merge branch 'cherry-pick-d5521047' into 'main'"
    This reverts commit f5897974bc41511cf1ea4e8996dc0db2b02cf213.

--- server/src/test/java/server/api/BoardControllerTest.java
+++ server/src/test/java/server/api/BoardControllerTest.java
@@ -5,9 +5,6 @@ import commons.Card;
 import commons.Column;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import server.database.CardTagRepository;
 import server.services.BoardService;
@@ -20,12 +17,10 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
-@SpringBootTest
-class BoardControllerTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
-    @Autowired
-    private ApplicationEventPublisher applicationEventPublisher;
+class BoardControllerTest {
 
     private TestBoardRepository boardRepository;
     private TestColumnRepository columnRepository;
@@ -60,9 +55,6 @@ class BoardControllerTest {
         columnService = new ColumnService(columnRepository);
         cardService = new CardService(cardRepository, subtaskRepository);
         subtaskService = new SubtaskService(subtaskRepository);
-
-        boardService.setApplicationEventPublisher(applicationEventPublisher);
-        columnService.setApplicationEventPublisher(applicationEventPublisher);
 
         cardController = new CardController(cardService, columnService, subtaskService, cardRepository);
         columnController = new ColumnController(columnRepository, boardRepository, cardRepository, cardController,
